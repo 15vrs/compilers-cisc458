a0647f0d0e3b89bc77f46e780e00c090b38cd860
Declan Rowett <15dar4@queensu.ca>
2021-02-04 16:44:33 -0500

    updated test script to work with restructure

--- testSuite/runTests.sh
+++ testSuite/runTests.sh
@@ -3,9 +3,14 @@
 cd ../ptsrc
 make scanner
 cd ../testSuite
-for file in * ; do
-    if [ ${file: -3} == ".pt" ]
-    then
-        ssltrace "ptc -o1 -t1 -L ../ptsrc/lib/pt ${file}" ../ptsrc/lib/pt/scan.def -e > ${file%.*}.eOutput
-    fi
+for path in * ; do
+    [ -d "${path}" ] || continue # if not a directory, skip
+    dirname="$(basename "${path}")"
+    echo $dirname
+    for file in "$dirname"/*; do
+        if [ ${file: -3} == ".pt" ]
+        then
+            ssltrace "ptc -o1 -t1 -L ../ptsrc/lib/pt ${file}" ../ptsrc/lib/pt/scan.def -e > ${file%.*}.eOutput
+        fi
+    done
 done
