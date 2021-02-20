14762562bfb36126ed2fd2ef3754d77b72848f09
Mimi Hawke <15amh7@linux1.caslab.queensu.ca>
2021-02-20 15:37:40 -0500

    if and choose statements

---
+++ testSuite/runTests2.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+##### Run this script from the command line while in the testSuite directory #####
+
+cd ../ptsrc/parser
+make
+cd ../../testSuite
+for path in * ; do
+    [ -d "${path}" ] || continue # if not a directory, skip
+    dirname="$(basename "${path}")"
+    echo $dirname
+    for file in "$dirname"/*; do
+        if [ ${file: -3} == ".pt" ]
+        then
+            ssltrace "ptc -o2 -t2 -L ../ptsrc/lib/pt ${file}" ../ptsrc/lib/pt/parser.def -e > ${file%.*}.eOutput
+        fi
+    done
+done
