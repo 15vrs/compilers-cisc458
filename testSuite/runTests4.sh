cddde535586f2d52d6afe58b80f394f4da9adfee
Declan Rowett <15dar4@queensu.ca>
2021-04-05 18:36:07 -0400

    make script normal

--- testSuite/runTests4.sh
+++ testSuite/runTests4.sh
@@ -7,49 +7,36 @@
 cd ../ptsrc
 make
 cd ../testSuite/phase4
-if [ "$1" == "t" ]
-then
-    cd ../../Instructor\ Examples
-    for file in * ; do
+for path in * ; do
+    [ -d "${path}" ] || continue # if not a directory, skip
+    dirname="$(basename "${path}")"
+    echo $dirname
+    cd $dirname
+    for file in ./*; do
+        if [ ${file: -8} == ".eOutput" ] # delete ".eOutput" files
+        then
+            rm -f ${file}
+        elif [ ${file: -4} == ".out" ] # delete ".out" files
+        then
+            rm -f ${file}
+        elif [ ${file: -2} == ".s" ] # delete ".s" files
+        then
+            rm -f ${file}
+        fi
         if [ ${file: -3} == ".pt" ]
         then
-            cd ourResults
-            ptc -S -L ../../ptsrc/lib/pt "../${file}"
-            cd ..
+            if [ "$1" == "d" ]
+            then : # Just leave all *.eOutput files deleted
+            elif [ "$1" == "s" ] # create ".s" files
+            then
+                ptc -S -L ../../../ptsrc/lib/pt ${file}
+            elif [ "$1" == "i" ]
+            then
+                ssltrace "ptc -o4 -t4 -L ../../../ptsrc/lib/pt ${file}" ../../../ptsrc/lib/pt/coder.def -ei > ${file%.*}.eOutput
+            else # create ".eOutput" and ".out" files
+                ssltrace "ptc -o4 -t4 -L ../../../ptsrc/lib/pt ${file}" ../../../ptsrc/lib/pt/coder.def -e > ${file%.*}.eOutput
+            fi
         fi
     done
-else
-    for path in * ; do
-        [ -d "${path}" ] || continue # if not a directory, skip
-        dirname="$(basename "${path}")"
-        echo $dirname
-        cd $dirname
-        for file in ./*; do
-            if [ ${file: -8} == ".eOutput" ] # delete ".eOutput" files
-            then
-                rm -f ${file}
-            elif [ ${file: -4} == ".out" ] # delete ".out" files
-            then
-                rm -f ${file}
-            elif [ ${file: -2} == ".s" ] # delete ".s" files
-            then
-                rm -f ${file}
-            fi
-            if [ ${file: -3} == ".pt" ]
-            then
-                if [ "$1" == "d" ]
-                then : # Just leave all *.eOutput files deleted
-                elif [ "$1" == "s" ] # create ".s" files
-                then
-                    ptc -S -L ../../../ptsrc/lib/pt ${file}
-                elif [ "$1" == "i" ]
-                then
-                    ssltrace "ptc -o4 -t4 -L ../../../ptsrc/lib/pt ${file}" ../../../ptsrc/lib/pt/coder.def -ei > ${file%.*}.eOutput
-                else # create ".eOutput" and ".out" files
-                    ssltrace "ptc -o4 -t4 -L ../../../ptsrc/lib/pt ${file}" ../../../ptsrc/lib/pt/coder.def -e > ${file%.*}.eOutput
-                fi
-            fi
-        done
-        cd ..
-    done
-fi
+    cd ..
+done
